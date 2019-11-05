0288e8a61f2bddc78b37cf3c80f69d36029773e3
Marianne OConnor <marianneoco@gmail.com>
2019-11-05 15:02:07 +0000

    Failing test - expect new card to have balance of zero

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,10 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  it { is_expected.to be_instance_of(Oystercard) }
+
+  it "should have a balance of zero as a new card" do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq 0
+  end
 end
