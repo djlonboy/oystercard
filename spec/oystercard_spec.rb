f9bf9429c9d288e405953a5db580b34b7f47a104
Marianne OConnor <marianneoco@gmail.com>
2019-11-05 15:13:20 +0000

    Test to allow user to top up card failing

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,7 +4,11 @@ describe Oystercard do
   it { is_expected.to be_instance_of(Oystercard) }
 
   it "should have a balance of zero as a new card" do
-    oystercard = Oystercard.new
-    expect(oystercard.balance).to eq 0
+    expect(subject.balance).to eq 0
+  end
+
+  it "should allow the user to top up the card" do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
   end
 end
