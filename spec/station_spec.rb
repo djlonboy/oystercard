4b057415630b2511176ce5293ca57d79f8508560
Marianne OConnor <marianneoco@gmail.com>
2019-11-06 16:29:53 +0000

    Failing test - station should have a name

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,11 +1,19 @@
 require 'station'
 
 describe Station do
-  it { is_expected.to be_instance_of Station}
+  it "should create an instance of the station class" do
+    station = Station.new(1)
+    expect(station).to be_instance_of Station
+  end
 
   it "should have a zone" do
     station = Station.new(1)
-    expect(subject.zone).to eq 1
+    expect(station.zone).to eq 1
+  end
+
+  it "should have a name" do
+    station = Station.new("test", 1)
+    expect(station.name).to eq "test"
   end
 
 
